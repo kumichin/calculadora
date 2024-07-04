30208b0e29b69244d2824c4303313660b17d8332
Kumi <Kumikoaisaka@gmail.com>
2024-07-04 04:18:43 +0200

    calculadora version preliminal
    Esta es la version preliminal de esta fantastica calculadora. esta version preliminal decidi hacerla con eval() pero la versión definitiva que sacaré muy pronto hará los calculos de otra manera

---
+++ calculadora con eval()/script.js
@@ -0,0 +1,16 @@
+
+function añadir(valor){
+    document.getElementById("pantalla").value +=valor;
+}
+
+function borrar(){
+    document.getElementById("pantalla").value = "";
+}
+
+function calcular(){
+    const valorPantalla = document.getElementById("pantalla").value;
+    const resultado = eval(valorPantalla);
+    document.getElementById("pantalla").value = resultado;
+
+    
+}
